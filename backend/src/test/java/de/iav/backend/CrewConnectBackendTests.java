37888df49bff6c527db705e5499468350a26232d
Dirk Stadge <dirk_stadge@yahoo.de>
2023-09-13 09:07:35 +0200

    add backendTest for Code smell

--- backend/src/test/java/de/iav/backend/CrewConnectBackendTests.java
+++ backend/src/test/java/de/iav/backend/CrewConnectBackendTests.java
@@ -1,11 +1,23 @@
 package de.iav.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CrewConnectBackendTests {
 
 
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+        assertThat(context).isNotNull();
+    }
 
 }
+
