37c45c88d6c1dde62c95e43c4dfaf758cda5ae3d
Dirk Stadge <dirk_stadge@yahoo.de>
2023-09-22 13:38:12 +0200

    delete in application.properties backend all logging

--- backend/src/main/java/de/iav/backend/service/SailorService.java
+++ backend/src/main/java/de/iav/backend/service/SailorService.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @Service
 public class SailorService {
 
-    private static final Logger logger = LoggerFactory.getLogger(SailorService.class);
+   // private static final Logger logger = LoggerFactory.getLogger(SailorService.class);
 
     private final SailorRepository sailorRepository;
 
