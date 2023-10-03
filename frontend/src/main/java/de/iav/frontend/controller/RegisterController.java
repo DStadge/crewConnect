b44d298247f762991de3f7ae30d1ba1241879ede
DStadge <dirk_stadge@yahoo.de>
2023-10-03 20:34:22 +0200

    add fade on RegisterController backButton

--- frontend/src/main/java/de/iav/frontend/controller/RegisterController.java
+++ frontend/src/main/java/de/iav/frontend/controller/RegisterController.java
@@ -4,6 +4,7 @@ import de.iav.frontend.CrewConnectFrontendApplication;
 import de.iav.frontend.exception.CustomIOException;
 import de.iav.frontend.security.AppUserRequest;
 import de.iav.frontend.security.AuthService;
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,6 +16,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -27,6 +29,48 @@ public class RegisterController {
     public PasswordField passwordInput;
     @FXML
     public Label errorLabel;
+
+/*
+    @FXML
+    protected void onClickBackButton(ActionEvent event) {
+        // Erstelle einen FXMLLoader, um die FXML-Datei für die Login-Szene zu laden
+        FXMLLoader loader = new FXMLLoader(CrewConnectFrontendApplication.class.getResource("/de/iav/frontend/fxml/login-scene.fxml"));
+        Parent root;
+        try {
+            // Lade die FXML-Datei und erstelle ein Parent-Objekt daraus
+            root = loader.load();
+        } catch (IOException e) {
+            // Behandle eine mögliche IOException
+            throw new CustomIOException(e.toString());
+        }
+
+        // Erhalte die aktuelle Bühne (Stage) des Buttons
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Erstelle eine neue Szene mit dem geladenen Root (Login-Seite)
+        Scene scene = new Scene(root);
+
+        // Erstelle eine FadeTransition, um einen Fade-Out-Effekt für die aktuelle Szene zu erstellen
+        FadeTransition fadeOutTransition = new FadeTransition(Duration.seconds(1), stage.getScene().getRoot());
+        fadeOutTransition.setFromValue(1.0);
+        fadeOutTransition.setToValue(0.0);
+
+        // Setze den EventHandler, um die neue Szene nach dem Fade-Out zu setzen
+        fadeOutTransition.setOnFinished(e -> {
+            stage.setScene(scene); // Wechsle zur neuen Szene
+            stage.setTitle("Login Seite");
+
+            // Erstelle eine FadeTransition für den Fade-In-Effekt der neuen Szene
+            FadeTransition fadeInTransition = new FadeTransition(Duration.seconds(1), stage.getScene().getRoot());
+            fadeInTransition.setFromValue(0.0);
+            fadeInTransition.setToValue(1.0);
+            fadeInTransition.play(); // Starte den Fade-In-Effekt
+        });
+
+        fadeOutTransition.play(); // Starte den Fade-Out-Effekt
+    }
+*/
+
 
     @FXML
     protected void onClickBackButton(ActionEvent event) {
