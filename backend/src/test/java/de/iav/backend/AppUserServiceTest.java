4d7d5f40c113cf34800d85ec3ca6e3494cda0e4c
Dirk Stadge <dirk_stadge@yahoo.de>
2023-10-13 09:55:35 +0200

    clean Code

--- backend/src/test/java/de/iav/backend/AppUserServiceTest.java
+++ backend/src/test/java/de/iav/backend/AppUserServiceTest.java
@@ -11,17 +11,12 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
