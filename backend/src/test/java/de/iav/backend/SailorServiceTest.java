028d83530fd1f8f2704765018b0023ac69f7dbab
Dirk Stadge <dirk_stadge@yahoo.de>
2023-10-13 11:47:19 +0200

    add Tests

--- backend/src/test/java/de/iav/backend/SailorServiceTest.java
+++ backend/src/test/java/de/iav/backend/SailorServiceTest.java
@@ -1,5 +1,7 @@
 package de.iav.backend;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import de.iav.backend.model.Sailor;
 import de.iav.backend.repository.SailorRepository;
@@ -13,6 +15,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -28,9 +32,12 @@ class SailorServiceTest {
     @BeforeEach
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        sailorRepository = mock(SailorRepository.class);
+        sailorService = new SailorService(sailorRepository);
     }
+
     @Test
-    void testUpdateSailorById() {
+    void testUpdateSailorByIdSailorFound() {
         String sailorId = "123";
         Sailor existingSailor = new Sailor(
                 sailorId, "Paul", "Panzer", "Experte", LocalDate.of(2023, 10, 13));
@@ -38,38 +45,120 @@ class SailorServiceTest {
         Sailor updatedSailor = new Sailor(
                 sailorId, "NeuerVorName", "NeuerNachName", "Anfänger", LocalDate.of(2023, 2, 20));
 
-        Mockito.when(sailorRepository.findById(sailorId)).thenReturn(Optional.of(existingSailor));
-        Mockito.when(sailorRepository.save(updatedSailor)).thenReturn(updatedSailor);
+        // Mocken des Repository-Verhaltens
+        when(sailorRepository.findById(sailorId)).thenReturn(Optional.of(existingSailor));
+        when(sailorRepository.save(updatedSailor)).thenReturn(updatedSailor);
 
-        System.out.println(sailorService.updateSailorById(sailorId, updatedSailor));
+        // Die Methode aufrufen und das Ergebnis überprüfen
+        Sailor result = sailorService.updateSailorById(sailorId, updatedSailor);
+
+        assertNotNull(result);
+        assertEquals(updatedSailor, result);
+
+        // Überprüfen, ob die Repository-Methoden korrekt aufgerufen wurden
+        verify(sailorRepository, times(1)).findById(sailorId);
+        verify(sailorRepository, times(1)).save(updatedSailor);
+    }
+
+    @Test
+    void testListAllSailor() {
+        Sailor sailor1 = new Sailor("1", "Paul", "Panzer", "Anfänger", LocalDate.of(2023, 12, 13));
+        Sailor sailor2 = new Sailor("2", "Paule", "Panzer", "Experte", LocalDate.of(2023, 2, 14));
+        List<Sailor> sailorList = new ArrayList<>();
+        sailorList.add(sailor1);
+        sailorList.add(sailor2);
+
+        when(sailorRepository.findAll()).thenReturn(sailorList);
+
+        List<Sailor> result = sailorService.listAllSailor();
+
+        assertEquals(2, result.size());
+        assertEquals(sailor1, result.get(0));
+        assertEquals(sailor2, result.get(1));
+    }
+
+    @Test
+    void testListAllSailorEmpty() {
+        when(sailorRepository.findAll()).thenReturn(new ArrayList<>());
+
+        List<Sailor> result = sailorService.listAllSailor();
+
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void testAddSailor() {
+        Sailor sailorToAdd = new Sailor("1", "Paul", "Panzer", "Anfänger", LocalDate.of(2023, 11, 16));
+
+        when(sailorRepository.save(sailorToAdd)).thenReturn(sailorToAdd);
+
+        Sailor result = sailorService.addSailor(sailorToAdd);
+
+        assertEquals(sailorToAdd, result);
+    }
+
+    @Test
+    void testGetSailorById() {
+        String sailorId = "1";
+        Sailor expectedSailor = new Sailor(sailorId, "Paul", "Panzer", "Anfänger", LocalDate.of(2023, 4, 11));
+
+        when(sailorRepository.findById(sailorId)).thenReturn(Optional.of(expectedSailor));
+
+        Optional<Sailor> result = sailorService.getSailorById(sailorId);
+
+        assertTrue(result.isPresent());
+
+        assertEquals(expectedSailor, result.get());
+    }
+
+    @Test
+    void testGetSailorByIdSailorNotFound() {
+        String sailorId = "2";
+
+        when(sailorRepository.findById(sailorId)).thenReturn(Optional.empty());
+
+        Optional<Sailor> result = sailorService.getSailorById(sailorId);
+
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    void testDeleteSailorById() {
+        String sailorId = "1";
+
+        sailorService.deleteSailorById(sailorId);
+
+        verify(sailorRepository, times(1)).deleteById(sailorId);
+    }
+
+    @Test
+    void testUpdateSailorById() {
+        String sailorId = "1";
+        Sailor existingSailor = new Sailor(sailorId, "Vorname", "Nachname", "Erfahren", LocalDate.now());
+        Sailor updatedSailor = new Sailor(sailorId, "NeuerVorname", "NeuerNachname", "Anfänger", LocalDate.now());
+
+        when(sailorRepository.findById(sailorId)).thenReturn(Optional.of(existingSailor));
+        when(sailorRepository.save(updatedSailor)).thenReturn(updatedSailor);
 
         Sailor result = sailorService.updateSailorById(sailorId, updatedSailor);
 
         assertNotNull(result);
         assertEquals(updatedSailor, result);
 
-        Mockito.verify(sailorRepository, Mockito.times(1)).findById(sailorId);
-        Mockito.verify(sailorRepository, Mockito.times(1)).save(updatedSailor);
+        verify(sailorRepository, times(1)).findById(sailorId);
+        verify(sailorRepository, times(1)).save(updatedSailor);
     }
 
     @Test
     void testUpdateSailorByIdSailorNotFound() {
-        String sailorId = "123";
-        Sailor updatedSailor = new Sailor(
-                sailorId, "NeuerVorName", "NeuerNachName", "Anfänger", LocalDate.of(2023, 2, 20));
+        String sailorId = "1";
+        Sailor updatedSailor = new Sailor(sailorId, "NeuerVorname", "NeuerNachname", "Anfänger", LocalDate.now());
 
-        Mockito.when(sailorRepository.findById(sailorId)).thenReturn(Optional.empty());
+        when(sailorRepository.findById(sailorId)).thenReturn(Optional.empty());
 
-        assertThrows(NoSuchElementException.class, () -> {
-            try {
-                sailorService.updateSailorById(sailorId, updatedSailor);
-            } catch (NoSuchElementException e) {
-                assertTrue(e.getMessage().contains("Segler mit der ID: 123 wurde nicht gefunden!"));
-                throw e;
-            }
-        });
+        assertThrows(NoSuchElementException.class, () -> sailorService.updateSailorById(sailorId, updatedSailor));
 
-        Mockito.verify(sailorRepository, Mockito.times(1)).findById(sailorId);
-        Mockito.verify(sailorRepository, Mockito.never()).save(updatedSailor);
+        verify(sailorRepository, times(1)).findById(sailorId);
+        verify(sailorRepository, never()).save(updatedSailor);
     }
 }
